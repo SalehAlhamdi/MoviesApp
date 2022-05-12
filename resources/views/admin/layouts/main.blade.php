d4faddf7277c42e5e152dc7c7d7a914ad7f2cfdb
Saleh <salehalhamd5@gmail.com>
2022-05-12 07:52:10 +0300

    adding select 2 JavaScript, CSS links

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,8 +10,26 @@
 
     <title>@yield('title')</title>
 
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset("admin/vendor/jquery/jquery.min.js")}}"></script>
+    <script src="{{asset("admin/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset("admin/vendor/jquery-easing/jquery.easing.min.js")}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset("admin/js/sb-admin-2.min.js")}}"></script>
+
     <!-- Custom fonts for this template-->
     <link href="{{asset("admin/vendor/fontawesome-free/css/all.min.css")}}"  rel="stylesheet" type="text/css">
+
+    <!-- Select 2 package-->
+    <link rel="stylesheet" href="{{asset('select2/select2.min.css')}}">
+    <script src="{{asset('select2/select2.min.js')}}"></script>
+
+
+
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -234,16 +251,6 @@
     </a>
 
 
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{asset("admin/vendor/jquery/jquery.min.js")}}"></script>
-    <script src="{{asset("admin/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="{{asset("admin/vendor/jquery-easing/jquery.easing.min.js")}}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{asset("admin/js/sb-admin-2.min.js")}}"></script>
 </div>
 </body>
 
