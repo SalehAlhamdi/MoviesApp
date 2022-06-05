4056ace884ba2e648b647e3ab6b915d402b27578
Saleh <salehalhamd5@gmail.com>
2022-06-05 16:07:23 +0300

    change the path
    Move under the main path

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\AdminDashboard\Auth;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
--- app/Http/Controllers/Lib/FunctionsHelper.php
+++ app/Http/Controllers/Lib/FunctionsHelper.php
@@ -1,24 +1,17 @@
 <?php
 
-namespace App\Http\Controllers\AdminDashboard\Lib;
+namespace App\Http\Controllers\Lib;
 
+use App\Models\Genres;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use function public_path;
 
 class FunctionsHelper
 {
-    public function update_files(string $path,string $fileType,string $fileName,Request $request){
-
-        if ($request->hasFile($fileType)){
-            if (!file_exists($path.$request->image->getClientOriginalName())){
-                File::delete($path.$fileName);
-            }
-
-            $file=$request->file($fileType);
-            $fileName=time().'.'.$file->extension();
-            $file->move(public_path($path),$fileName);
-        }
-
+    public function all_years(){
+        $genres=Genres::all();
+        $yeras=array_unique($genres->years);
+        return $yeras;
     }
 }
